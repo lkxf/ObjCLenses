a4fb473df5d625990f9917cdcf2e735acf303c91
Lk Xf <elveestei@gmail.com>
2014-11-15 22:36:49 +0100

    Initial Commit

---
+++ ObjCLenses/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCLenses
+//
+//  Created by Tamas Lustyik on 2014.11.15..
+//  Copyright (c) 2014 Tamas Lustyik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
