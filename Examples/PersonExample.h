6772c5a2427d4f24e513bd77486650d0bd63eb74
Lk Xf <elveestei@gmail.com>
2014-11-16 19:24:53 +0100

    Refactored examples

---
+++ Examples/PersonExample.h
@@ -0,0 +1,12 @@
+//
+//  PersonExample.h
+//  ObjCLenses
+//
+//  Created by Tamas Lustyik on 2014.11.16..
+//  Copyright (c) 2014 Tamas Lustyik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern void PersonExample();
+
