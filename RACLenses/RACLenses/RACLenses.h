f525feff6a678be592ffa24639887f4c4f7936d2
Lk Xf <elveestei@gmail.com>
2014-11-24 00:05:16 +0100

    Created RAC extension project

---
+++ RACLenses/RACLenses/RACLenses.h
@@ -0,0 +1,19 @@
+//
+//  RACLenses.h
+//  RACLenses
+//
+//  Created by Tamas Lustyik on 2014.11.23..
+//  Copyright (c) 2014 Tamas Lustyik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RACLenses.
+FOUNDATION_EXPORT double RACLensesVersionNumber;
+
+//! Project version string for RACLenses.
+FOUNDATION_EXPORT const unsigned char RACLensesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RACLenses/PublicHeader.h>
+
+
