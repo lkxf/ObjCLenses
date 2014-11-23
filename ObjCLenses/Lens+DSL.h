3158562b9054ab7253df04b8c2ffdac9de8f10fd
Lk Xf <elveestei@gmail.com>
2014-11-23 20:55:54 +0100

    Created a preliminary DSL for lenses

---
+++ ObjCLenses/Lens+DSL.h
@@ -0,0 +1,30 @@
+//
+//  Lens+DSL.h
+//  ObjCLenses
+//
+//  Created by Tamas Lustyik on 2014.11.23..
+//  Copyright (c) 2014 Tamas Lustyik. All rights reserved.
+//
+
+#import "Lens.h"
+
+Lens* Focus();
+
+@interface Lens (DSL_Array)
+@property (nonatomic, strong, readonly) Lens* (^onFirst)();
+@property (nonatomic, strong, readonly) Lens* (^onLast)();
+@property (nonatomic, strong, readonly) Lens* (^onIndex)(NSInteger index);
+@end
+
+@interface Lens (DSL_Dictionary)
+@property (nonatomic, strong, readonly) Lens* (^onKey)(id<NSCopying> key);
+@end
+
+@interface Lens (DSL_KVC)
+@property (nonatomic, strong, readonly) Lens* (^onKeyPath)(NSString* keyPath);
+@end
+
+@interface Lens (DSL_Struct)
+@property (nonatomic, strong, readonly) Lens* (^onStructKeyPath)(StructKeyPath skp);
+@end
+
---
+++ ObjCLenses/Lens+DSL.m
@@ -0,0 +1,82 @@
+//
+//  Lens+DSL.m
+//  ObjCLenses
+//
+//  Created by Tamas Lustyik on 2014.11.23..
+//  Copyright (c) 2014 Tamas Lustyik. All rights reserved.
+//
+
+#import "Lens+DSL.h"
+
+
+Lens* Focus() {
+    return [Lens new];
+}
+
+@implementation Lens (DSL_Array)
+
+- (Lens *(^)())onFirst {
+    __weak Lens* weakSelf = self;
+    return ^Lens*() {
+        __strong Lens* self = weakSelf;
+        return [self lensByAppendingLens:[Lens lensToFirstItem]];
+    };
+}
+
+- (Lens *(^)())onLast {
+    __weak Lens* weakSelf = self;
+    return ^Lens*() {
+        __strong Lens* self = weakSelf;
+        return [self lensByAppendingLens:[Lens lensToLastItem]];
+    };
+}
+
+- (Lens *(^)(NSInteger))onIndex {
+    __weak Lens* weakSelf = self;
+    return ^Lens*(NSInteger idx) {
+        __strong Lens* self = weakSelf;
+        return [self lensByAppendingLens:[Lens lensToItemAtIndex:idx]];
+    };
+}
+
+@end
+
+
+@implementation Lens (DSL_Dictionary)
+
+- (Lens *(^)(id<NSCopying>))onKey {
+    __weak Lens* weakSelf = self;
+    return ^Lens*(id<NSCopying> key) {
+        __strong Lens* self = weakSelf;
+        return [self lensByAppendingLens:[Lens lensToKey:key]];
+    };
+}
+
+@end
+
+
+@implementation Lens (DSL_KVC)
+
+- (Lens *(^)(NSString*))onKeyPath {
+    __weak Lens* weakSelf = self;
+    return ^Lens*(NSString* keyPath) {
+        __strong Lens* self = weakSelf;
+        return [self lensByAppendingLens:[Lens lensToKeyPath:keyPath]];
+    };
+}
+
+@end
+
+
+@implementation Lens (DSL_Struct)
+
+- (Lens *(^)(StructKeyPath))onStructKeyPath {
+    __weak Lens* weakSelf = self;
+    return ^Lens*(StructKeyPath skp) {
+        __strong Lens* self = weakSelf;
+        return [self lensByAppendingLens:[Lens lensToStructKeyPath:skp]];
+    };
+}
+
+@end
+
